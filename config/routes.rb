88f7f7ccc0b90db7b455965176599a2c2b340a92
shohei hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-06-16 15:44:45 +0900

    change: ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'plans/new'
-  get 'plans/index'
-  get 'plans/show'
-  get 'plans/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :plans
 end
