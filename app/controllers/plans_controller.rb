65b3ff8bc0f7539c836075363405a8253b9fda17
shohei hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-06-21 13:28:42 +0900

    add: プラン作成機能の雛形追加

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,15 @@
 class PlansController < ApplicationController
   def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to plan_url(@plan.id), notice: "プランを作成しました。"
+    else
+      render :new
+    end
   end
 
   def index
@@ -9,5 +19,11 @@ class PlansController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:area, :impression, :image)
   end
 end
