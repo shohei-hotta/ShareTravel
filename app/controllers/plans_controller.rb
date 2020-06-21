74892931b423ceecb68950029e600dbeab2cbe05
shohei hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-06-21 16:45:34 +0900

    add: プラン編集機能を追加

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,6 @@
 class PlansController < ApplicationController
+  before_action :set_plan, only: [:show, :edit, :update]
+
   def new
     @plan = Plan.new
     2.times { @plan.places.build }
@@ -17,11 +19,19 @@ class PlansController < ApplicationController
   end
 
   def show
-    @plan = Plan.find(params[:id])
     @places = @plan.places
   end
 
   def edit
+    @plan.places.build
+  end
+
+  def update
+    if @plan.update(plan_params)
+      redirect_to plan_url(@plan.id), notice: "プランを編集しました。"
+    else
+      render :edit
+    end
   end
 
   private
@@ -29,5 +39,9 @@ class PlansController < ApplicationController
   def plan_params
     params.require(:plan).permit(:area, :impression, :image, :image_cache, :published,
                                   places_attributes: [:id, :name, :latitude, :longitude, :image, :image_cache, :explanation, :_destroy])
+  end
+
+  def set_plan
+    @plan = Plan.find(params[:id])
   end
 end
